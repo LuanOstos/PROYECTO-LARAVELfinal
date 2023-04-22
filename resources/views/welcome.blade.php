467e1c05d687fb320a0ecd0eaf5bd97d31748ffd
vboxuser <luanmelieostos@gmail.com>
2023-04-21 23:58:32 -0500

    DW-LARAVELFinal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>TURISMO</title>
+    <style>
+        
+        h1{
+            text-align:center;
+            height: 100px;
+            margin: 30px;           
+        }
+        body{
+            
+            background-color:SeaShell;
+        }
+        footer{
+            margin: 50px;
+        }
+    </style>
+
+   
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="btn btn navbar-brand" href="#">ViajesPerú</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                    
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <style> .btn {
+                                        margin-right: 100px;
+            
+                            }
+                            </style>
+                            <a class="btn btn-primary nav-link" href="https://www.youtube.com/watch?v=OL9hn_kgy6s&t=162s">COSTA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-info nav-link" href="https://www.youtube.com/shorts/fpel43M6eeQ">SIERRA</a>
+                        <li class="nav-item">
+                            <a class="btn btn-success nav-link" href="https://www.youtube.com/watch?v=bPzn6AsbARk">SELVA</a>    
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-secondary nav-link" href="https://touristear.com/itinerario-visitar-peru-12-dias/">GUIA-Tours</a>
+                        <li>
+                            <ul>
+                            <button data-bs-toggle="collapse" data-bs-target="#demo">WhatsApp</button>
+
+<div id="demo" class="collapse">
+<a href="https://api.whatsapp.com/send?phone=51917218363&text=Hola, Nececito mas informacion!">Envíanos un mensaje de WhatsApp</a>
+</div>
+ 
+                        </div>
+                            </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div>
+            <h1>VACACIONES EN PERU</h1>
+        </div>
+        <!-- carrusel-->
+        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Indicators/dots -->
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+                </div>
+                
+                <!-- The slideshow/carousel -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="https://maestrovirtuale.com/wp-content/uploads/2019/10/region-chala-costa-del-peru.jpg" alt="COSTA" class="d-block" style="width:100%">
+                    <div class="carousel-caption">
+                        <h3>COSTA</h3>
+                        <p>Es una zona altamente turística por la cantidad de playas, islas e islotes que posee y por la gran cantidad de reservas naturales, a través de las cuales se protegen varios organismos vegetales y animales importantes para el ecosistema de la costa peruana.</p>
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="https://www.peru.travel/Contenido/General/Imagen/es/302/1.1/Vinicunca.jpg" alt="SIERRA" class="d-block" style="width:100%">
+                    <div class="carousel-caption">
+                        <h3>SIERRA</h3>
+                        <p>El colorido de Vinicunca se debe a una formación geológica acumulada a través del tiempo por la mezcla de elementos marinos, lacustres y fluviales. Esta reunión de minerales arrastrados por las aguas de la cordillera, los vientos y la humedad, ha causado la oxidación del lugar y ha hecho que la montaña adquiera esa diversidad mágica de tonalidades.</p>
+                    </div> 
+                    </div>
+                    <div class="carousel-item">
+                    <img src="https://historia.nationalgeographic.com.es/medio/2014/09/15/129orellana1_2000x1327.jpg" alt="SELVA" class="d-block" style="width:100%" >
+                    <div class="carousel-caption">
+                        <h3>SELVA</h3>
+                        <p>El río Amazonas y la Amazonía del Perú son ideales para aficionados a la naturaleza y la biodiversidad; observadores de aves, flora y fauna; interesados en el misticismo y las tradiciones populares y para los apasionados por navegar en sus ríos.</p>
+                    </div>  
+                    </div>
+                </div>
+                <br>
+        
+        <!-- Left and right controls/icons -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+        </div>
+
+
+        
+        <div class="container-sm">
+                <div class="row">
+                    <!--25% or 3/12 -->
+                    <div class="col-5">
+                        <video  width="510px" height="470px" autoplay muted>
+                        <source src="https://www.machupicchu.gob.pe/wp-content/uploads/2021/08/mapi_web.webm" type="video/mp4">
+                        </video>
+                    
+                         
+                    </div>
+
+                    
+                    <div class="col-7">
+                        <p><h2 style="text-align:center;">CUSCO</h2></p>
+                        <p>Fue conocida durante la colonia como Picchu, Piccho o Picho, aunque algunos sostienen que pudo haberse llamado Patallaqta (pueblo en lo alto). La ciudadela fue redescubierta por Hiram Bingham en 1911 y se llevó miles de piezas que hasta hoy permanecen en la Universidad de Yale, en Estados Unidos. Felizmente algunas ya regresaron al Cusco el 2011. Cabe destacar que The Huffington Post eligió a Machu Picchu como el primer destino a visitar antes de morir. Revisa las tarifas de ingreso a Machu Picchu más abajo.<br>Machu Picchu es una de las 7 maravillas del mundo más visitadas por los turistas, posee hermosas construcciones a base de piedras, que fueron talladas con mucha precisión y detalle, es la obra más importante para los incas por haber sido construida en una montaña agreste e inaccesible, dividida en dos grandes sectores, urbano y agrícola separados por una gran muralla que desciende por la ladera del cerro hasta llegar a las orillas del rio Vilcanota.</p>
+                        <p style="text-align:center;"><a href="https://www.machupicchu.gob.pe/" target="_blank"><button class="btn btn-secondary btn btn-outline-dark"><span class="spinner-grow spinner-grow-sm"></span>Visitar página</button></a></p>
+                    </div>
+
+                </div>
+        </div>
+        <div class="container-sm" >
+                <div class="row" >
+                    <div class="col-7">
+                        <p><h2 style="text-align:center;">SELVA</h2></p>
+                        <p>En el lejano Este del Perú, allá donde concluyen las últimas estribaciones de la Cordillera de los Andes.se inicia la región de la Selva Baja, el gran llano amazónico, la floresta gigantesca e indomable, la región de los ríos—mares (el Amazonas, el Ucayali, el Marañón, el Urubamba, el Madre de Dios, el Ñapo, el Putumayo, etc.) que representa cerca del 50 % del territorio nacional.
+                            En esta selva de gigantescos bosques vírgenes, no hay caminos. Mejor dicho, los caminos son los ríos, que abren las puertas del Perú hacia el Atlántico, pues los barcos de gran calado ingresan por el Amazonas—el río más caudaloso del mundo— hasta el puerto de Iquitos, capital del Departamente de Loreto, el más extenso del país.<br>La selva peruana inicia en las estribaciones orientales de la Cordillera Andina, y se extiende hasta las fronteras políticas peruanas con Brasil, Colombia y Ecuador, en el territorio de los departamentos peruanos de Loreto, Amazonas, San Martín, Ucayali y Madre de Dios. Todo ello, claro está, en el centro-oeste del subcontinente sudamericano.
+                            Se divide en selva de montaña o selva alta (en la cordillera) y selva tropical o selva baja (en la llanura y el piedemonte)
+                        </p>
+                        <p style="text-align:center;"><a href="https://touristear.com/visita-la-selva-peruana/" target="_blank"><button class="btn btn-secondary btn btn-outline-info"><span class="spinner-grow spinner-grow-sm"></span>Visitar página</button></a></p>
+                    </div>
+                    <div class="col-5">
+                        <img src="https://www.machupicchu-tours-peru.com/wp-content/uploads/2014/03/amazonas-2.jpg" width="510px" height="410px" alt="SELVA">              
+                    </div>
+                </div>
+        </div>
+        <div class="container-sm" style="margin-top: 60px;">
+                <div class="row">
+                    <!--25% or 3/12 -->
+                    <div class="col-5">
+                        <img src="https://seturismo.pe/wp-content/uploads/2018/08/Plaza-de-Armas-y-Catedral-de-Arequipa.jpg" width="510px" height="470px" alt="CIUDAD BLANCA">
+                    </div>
+
+                    
+                    <div class="col-7">
+                        <p><h2 style="text-align:center;">AREQUIPA</h2></p>
+                        <P>El departamento de Arequipa es uno de los 24 departamentos que conforman la República Constitucional del Perú y cuya capital es su homónima ciudad o también conocida como la “Ciudad Blanca”. Arequipa es considerada como una de las ciudades más importantes del Perú, llegando a ser declarada por la Unesco como “Patrimonio Cultural de la Humanidad” debido a su increíble arquitectura colonial.<br>La Catedral de la ciudad de Arequipa, al costado de la Plaza de Armas, es una de las primeras construcciones religiosa que se crearon en esta ciudad. Su construcción presenta un estilo neoclásico hecho a base de sillar. En el interior de este recinto se puede observar un enorme órgano de origen belga de incalculable valor. También se debe destacar que es una de las 70 iglesias del mundo que puede izar el estandarte del Vaticano.<br>Una característica poco convencional y muy impactante de esta tierra es que se encuentra rodeada de 4 volcanes: Ampato (6,288 metros sobre el nivel del mar), Chachani (6, 075 metros sobre el nivel del mar), Misti (5,825 metros sobre el nivel del mar),  y Pichu Pichu (5,644  metros sobre el nivel del mar).</P>
+                        <p style="text-align:center;"><a href="https://www.machupicchu.gob.pe/" target="_blank"><button class="btn btn-secondary btn btn-outline-danger"><span class="spinner-grow spinner-grow-sm"></span>Visitar página</button></a></p>
+                    </div>
+                </div>
+        </div>
+        <div style="margin-top:30px;">
+            <div>
+            <nav>
+                    <h2 style="text-align:center;">PAISAJES</h2><br>
+                    <section >
+                            <style>
+                            section{
+                                display: flex;
+                                width: 900px;
+                                height:380px;
+                                margin: auto;
+                                
+                            }
+
+                            section img{
+                            width: 0px;
+                            flex-grow:1;
+                            object-fit: cover;
+                            opacity: .8;
+                            transition: .5s ease;
+                            
+                            
+                            }
+                            section img:hover{
+                            cursor: crosshair;
+                            width:300px;
+                            opacity: 1;
+                            filter: contrast(120%);
+                            
+                            }
+                                
+                            </style>
+                            
+                            <img src="https://cdn.vallarta-adventures.com/sites/default/files/2019-01/cancun-about-weather%20%281%29.jpg"></img>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Machu_Picchu%2C_Peru_%282018%29.jpg"></img>
+                            <img src="https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/08/23173150/sacsayhuaman-1024x576.jpg"></img>
+                            <img src="https://imgmedia.larepublica.pe/640x377/larepublica/migration/images/BVCA7JCDVRA5XP5Z6E2GSH5B6Q.webp"></img>
+                            <img src="https://movistarplus.pe/wp-content/uploads/sites/3/2021/08/NW-02.jpg">
+                            <img src="https://e.rpp-noticias.io/normal/2019/09/05/185918_836725.jpg">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-QXm0xwY9M2auKNkMFIw9f3HWe9LtZb-Jw&usqp=CAU">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTLibIFC2AOKwWfZfxp1d5_wmFQ6idGqMj1Q&usqp=CAU">
+                            <img src="https://cdn.www.gob.pe/uploads/document/file/2386646/standard_WhatsApp%20Image%202021-11-10%20at%208.54.45%20AM.jpeg.jpeg">
+                    </section>
+            </nav>          
+
+            </div>
+        </div>  
+    </body>
+    <footer>
+    </footer>
+</html>
